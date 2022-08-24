d41874016adcc509ad77850cd1c47ce5f36cee32
Alongkorn Thepwong <oyasumi.7w7@gmail.com>
2022-08-24 16:31:31 +0700

    asdasd

--- src/api.js
+++ src/api.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const instance = axios.create({
 //    baseURL: 'http://localhost:8080',
  baseURL: 'fit2b-backend.vercel.app',
-  withCredentials: true, 
+  //withCredentials: true, 
   // เพื่อให้ส่ง cookie ได้
 });
